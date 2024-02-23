a5e21e364beea855a774ce27c0e5f71a59f925b3
chdrchz <saavannaa.rose@gmail.com>
2024-02-22 18:16:37 -0600

    reverting changes - it did not work

--- models/base_model.py
+++ models/base_model.py
@@ -7,19 +7,20 @@ from datetime import datetime
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
+        """Instatntiates a new model"""
         if not kwargs:
-            kwargs = {}
-        kwargs.setdefault('id', str(uuid4()))
-        kwargs.setdefault('created_at', datetime.utcnow())
-        if not isinstance(kwargs['created_at'], datetime):
-            kwargs['created_at'] = datetime.strptime(
-                kwargs['created_at'], "%Y-%m-%d %H:%M:%S.%f"
-            )
-        kwargs.setdefault('updated_at', datetime.utcnow())
-        if not isinstance(kwargs['updated_at'], datetime):
-            kwargs['updated_at'] = datetime.strptime(
-                kwargs['updated_at'], "%Y-%m-%d %H:%M:%S.%f"
-            )
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
