316edacb27d291bba97a1644628062d799466c58
chdrchz <saavannaa.rose@gmail.com>
2024-02-26 13:38:26 -0600

    testing changes to state

--- models/state.py
+++ models/state.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
-from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+
+STO_TYP = getenv("HBNB_TYPE_STORAGE")
+
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", 
-                          cascade="all, delete")
-    @property
-    def cities(self):
-        """" Returns the list of City instances with state_id. """
-        import models
-        from models.city import City
-        city_list = []
-        all_cities = models.storage.all(models.City)
-        for city in all_cities.values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+    if STO_TYP == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="delete")
+    else:
+        name = ''
+
+        @property
+        def cities(self):
+            """
+                getter method, returns list of City objs from storage
+                linked to the current State
+            """
+            city_list = []
+            for city in models.storage.all("City").values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
