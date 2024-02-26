c123be3f18c676344d24809307f77dac7bb58792
natewood2 <nathanwood.sfg@gmail.com>
2024-02-25 21:51:09 -0600

    Pushing so I don't lose progress.

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """ Returns the list of City instances with state_id. """
+            from models import storage
+            from models.city import City
+            all_cities = storage.all(City)
+            state_cities = [city for city in all_cities.values() if city.state_id == self.id]
+            return state_cities
