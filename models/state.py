49eac63b47c632f97bd897f89e30262c3cea334a
natewood2 <nathanwood.sfg@gmail.com>
2024-02-25 22:04:42 -0600

    Forgot to add ,Base to both

--- models/state.py
+++ models/state.py
@@ -5,7 +5,7 @@ from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
