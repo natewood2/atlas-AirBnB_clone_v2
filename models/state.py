2b22e9d864dac60ca5d25b4d0f61c4c5b644d764
natewood2 <nathanwood.sfg@gmail.com>
2024-02-26 06:54:03 -0600

    Restructing state.py

--- models/state.py
+++ models/state.py
@@ -9,15 +9,16 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
-    else:
-        @property
-        def cities(self):
-            """ Returns the list of City instances with state_id. """
-            from models import storage
-            from models.city import City
-            all_cities = storage.all(City)
-            state_cities = [city for city in all_cities.values() if city.state_id == self.id]
-            return state_cities
+    cities = relationship("City", backref="state", 
+                          cascade="all, delete, delete-orphan")
+    @property
+    def cities(self):
+        """" Returns the list of City instances with state_id. """
+        import models
+        from models.city import City
+        city_list = []
+        all_cities = models.storage.all(models.City)
+        for city in all_cities.values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
