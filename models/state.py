542530f617e57884a7939054a8bdad7e7287fe9a
natewood2 <nathanwood.sfg@gmail.com>
2024-02-27 10:41:42 -0600

    TESTING

--- models/state.py
+++ models/state.py
@@ -11,21 +11,18 @@ STO_TYP = getenv("HBNB_TYPE_STORAGE")
 
 class State(BaseModel, Base):
     """ State class """
-    if STO_TYP == 'db':
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="delete")
-    else:
-        name = ''
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
 
-        @property
-        def cities(self):
-            """
-                getter method, returns list of City objs from storage
-                linked to the current State
-            """
-            city_list = []
-            for city in models.storage.all("City").values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    @property
+    def cities(self):
+        """
+            getter method, returns list of City objs from storage
+            linked to the current State
+        """
+        city_list = []
+        for city in models.storage.all("City").values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
