ab07224e45a3844a80ebbc311b906a8ccf1e2645
chdrchz <saavannaa.rose@gmail.com>
2024-02-27 18:19:41 -0600

    adding environment to amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -3,8 +3,14 @@
 from models.base_model import BaseModel, Base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from os import getenv
+STO_TYP = getenv('HBNB_TYPE_STORAGE')
 
 
 class Amenity(BaseModel, Base):
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
+    if STO_TYP == "db":
+        from models.place import place_amenity
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ''
