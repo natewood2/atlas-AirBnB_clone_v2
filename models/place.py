440e0968eb23c6800e8dd8cb2e5164837a5cda1e
chdrchz <saavannaa.rose@gmail.com>
2024-02-27 21:41:04 -0600

    quick changes

--- models/place.py
+++ models/place.py
@@ -19,17 +19,14 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
 
-    # Establishing the relationship with Amenity
     amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
 
-    # For DBStorage
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         place_amenity = Table('place_amenity', Base.metadata,
                               Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
                               Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
                               )
 
-    # For FileStorage
     if os.getenv('HBNB_TYPE_STORAGE') == 'file':
         @property
         def amenities(self):
