9e95641b375441e6c234d65fb45ddc2e4dd23e10
chdrchz <saavannaa.rose@gmail.com>
2024-02-27 21:07:39 -0600

    I set the environment to only be accessible if it is db, testing

--- models/place.py
+++ models/place.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy import Table
+from os import getenv
 
-place_amenity = Table(
-    'place_amenity',
-    Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
-)
+STO_TYP = getenv("HBNB_TYPE_STORAGE")
+
+if STO_TYP == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True,
+                                 nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True,
+                                 nullable=False))
 
 class Place(BaseModel, Base):
     """ Place inherits from BaseModel and Base and a nice place to stay. """
