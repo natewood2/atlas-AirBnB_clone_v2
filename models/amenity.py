5b9b30434a6fb1d964f87cdc34a754f739b56fb4
chdrchz <saavannaa.rose@gmail.com>
2024-02-27 18:20:13 -0600

    reverting changes

--- models/amenity.py
+++ models/amenity.py
@@ -3,14 +3,8 @@
 from models.base_model import BaseModel, Base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from os import getenv
-STO_TYP = getenv('HBNB_TYPE_STORAGE')
 
 
 class Amenity(BaseModel, Base):
-    if STO_TYP == "db":
-        from models.place import place_amenity
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ''
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
