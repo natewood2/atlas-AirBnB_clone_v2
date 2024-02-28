b2939fad7bfcd9ce386a0c5b148d5a2479e11b2c
chdrchz <saavannaa.rose@gmail.com>
2024-02-27 21:17:36 -0600

    remvinig relationship to see if delete is fixed

--- models/place.py
+++ models/place.py
@@ -6,16 +6,6 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy import Table
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id'),
-                             primary_key=True,
-                             nullable=False),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'),
-                             primary_key=True,
-                             nullable=False))
-
 class Place(BaseModel, Base):
     """ Place inherits from BaseModel and Base and a nice place to stay. """
     __tablename__ = 'places'
@@ -29,9 +19,3 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-
-    amenities = relationship('Amenity', secondary=place_amenity, viewonly=False)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.amenity_ids = []
