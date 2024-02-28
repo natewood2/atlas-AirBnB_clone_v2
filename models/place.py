ab10c3f85c370d06b6262d13fca44767bb7c541d
chdrchz <saavannaa.rose@gmail.com>
2024-02-27 21:09:15 -0600

    removing conditional if it is db

--- models/place.py
+++ models/place.py
@@ -9,16 +9,15 @@ from os import getenv
 
 STO_TYP = getenv("HBNB_TYPE_STORAGE")
 
-if STO_TYP == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id'),
-                                 primary_key=True,
-                                 nullable=False),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id'),
-                                 primary_key=True,
-                                 nullable=False))
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True,
+                             nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True,
+                             nullable=False))
 
 class Place(BaseModel, Base):
     """ Place inherits from BaseModel and Base and a nice place to stay. """
