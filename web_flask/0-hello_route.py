e4700f6bd81b3c6d6df2847116824849c93edd2f
natewood2 <nathanwood.sfg@gmail.com>
2024-03-10 06:41:34 -0500

    Took away a ',' after Hello

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    return "Hello, HBNB!" 
+    return "Hello HBNB!" 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
