3715ff688c684bfb5575002ed7cdba3dde8278f8
natewood2 <nathanwood.sfg@gmail.com>
2024-03-10 06:44:15 -0500

    Corrected PyCode

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    return "Hello HBNB!" 
+    return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
