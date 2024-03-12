c3e3f607790fd13310756d871b42f8806c8d1951
natewood2 <nathanwood.sfg@gmail.com>
2024-03-12 13:19:49 -0500

    Addin 6 and the html for 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Web Flask which is a WSGI. """
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_and_text(text):
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_and_text_and_otherStuff(text='is cool'):
+    return "Python " + text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n: int):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n: int):
+    v = 'even' if n % 2 == 0 else 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, v=v)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
