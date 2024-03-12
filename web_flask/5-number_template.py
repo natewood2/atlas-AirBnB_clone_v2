e32d75f50b17b241a4aceed2e0a4c5291b0ed1f7
natewood2 <nathanwood.sfg@gmail.com>
2024-03-12 13:04:52 -0500

    Forgot to add the proper html file in my .py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,8 +33,8 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('number.html', n=n)
+def number_template(n: int):
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
