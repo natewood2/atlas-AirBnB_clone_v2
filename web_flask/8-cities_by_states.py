3cabac343e430e231d53f4e63e6532fa4db3a5e7
natewood2 <nathanwood.sfg@gmail.com>
2024-03-12 14:52:57 -0500

    I bleieve this will fix my issue

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """ Will help display on the front end. """
 from models.__init__ import storage
-from models.state import State
+from models import State
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def close(error):
-    storage.close()
-
-
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states_list():
     states = storage.all(State)
     return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def close(error):
+    storage.close()
 
 
 if __name__ == "__main__":
