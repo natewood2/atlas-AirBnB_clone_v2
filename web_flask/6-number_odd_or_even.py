bc66d5defc65b1cf75dd187d92077d002cc38809
natewood2 <nathanwood.sfg@gmail.com>
2024-03-12 13:20:26 -0500

    Addin 6 and the html for 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -42,5 +42,6 @@ def number_odd_or_even(n: int):
     v = 'even' if n % 2 == 0 else 'odd'
     return render_template('6-number_odd_or_even.html', n=n, v=v)
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
